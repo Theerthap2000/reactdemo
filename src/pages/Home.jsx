0b8f7e14af1f142588094506f293e27656eb1d9f
Theerthap2000 <theerthapramod@gmail.com>
2023-07-05 21:53:38 +0530

    new commit3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import img1 from '../assets/_IDNIGHT.jpg'
+import './Home.css'
+import img1 from '../assets/abc.jpg'
 const Home = () => {
     return (
         <>
             <div className="main">
-                <div className="image">
-                    <img src={img1} />
+                <div className="nav">
+                    <div className="image">
+                        <img src={img1} />
 
+                    </div>
+                    <div className="sp">Spotify'</div>
+                    <div className="a">Permision</div>
+                    <div className="b">Help</div>
+                    <div className="c">Download</div>
+                    <div className="f">|</div>
+                    <Link to={"/signup"}>
+                        <div className="d">Sign up</div>
+                    </Link>
+                    <Link to={"/login"}>
+                        <div className="e">Login</div>
+                    </Link>
+                </div >
+                <div className='container'>
+
+                    <div className="one">Music for everyone.</div>
+                    <div className="two">Millions of songs. No credit card needed.</div>
+                    <div className="button">
+                        <button>GET SPOTIFY FREE</button>
+                    </div>
                 </div>
-                <div className="a">Permision</div>
-                <div className="b">Help</div>
-                <div className="c">Download</div>
-                <div className="d">Sign up</div>
-                <div className='f'>home page</div>
-                <div className="e">Login</div>
             </div>
+
         </>
     )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import './Login.css'
 const Login = () => {
     return (
-        <div>Login</div>
+        <>
+            <div className="containe">
+                <div className="contain">
+                    <div className="s1">Login</div>
+                    <form>
+                        <label htmlFor="" className='em'>Email:</label>
+                        <input type="email" placeholder='Enter email' className='email' />
+                        <label htmlFor='' className='pas'>Password</label>
+                        <input type='password' placeholder='enter password' className='pass' />
+                        <label className='s3'><input type='checkbox' className='s2' />Show password</label>
+                        <input type='submit' name="submit" value="SIGN IN" className='signin'></input>
+                        <div className='for'>Forgot<a href='' className='user'>Username/password</a></div>
+                        <div className='don'>Don't have an account<a href='Signup' className='sig'>Sign up</a></div>
+                    </form>
+                </div>
+
+            </div>
+        </>
     )
 }
 
